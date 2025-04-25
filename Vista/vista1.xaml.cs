a34aa322af12f5c575e296bf1d9fd3815025fd5c
Byron Guallasamin <bguallasamin1999@outlook.com>
2025-04-24 21:48:14 -0500

    Agregar archivos de proyecto.

---
+++ Vista/vista1.xaml.cs
@@ -0,0 +1,9 @@
+namespace bguallasaminTS3.Vista;
+
+public partial class vista1 : ContentPage
+{
+	public vista1()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Vista/vista2.xaml.cs
@@ -0,0 +1,9 @@
+namespace bguallasaminTS3.Vista;
+
+public partial class vista2 : ContentPage
+{
+	public vista2()
+	{
+		InitializeComponent();
+	}
+}
